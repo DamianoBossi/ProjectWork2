c9c48f1c7e8f2b92b1f7d981993494eb0417aacf
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-11-10 17:29:15 +0100

    connection

--- tech-hub/src/main/java/it/zucchetti/packages/jdbc/JDBCConnection.java
+++ tech-hub/src/main/java/it/zucchetti/packages/jdbc/JDBCConnection.java
@@ -1,8 +1,8 @@
 package it.zucchetti.packages.jdbc;
 
-public class JDBCConnection { 
+public class JDBCConnection {
     public static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static final String CONNECTION_STRING = "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;encrypted=true;trustServerCertificate=true;"; 
+    public static final String CONNECTION_STRING = "jdbc:sqlserver://localhost:1433;databaseName=pw2;encrypted=true;trustServerCertificate=true;"; 
     public static final String USER = "TechHub";
     public static final String PASSWORD = "Segretone123";
 }
